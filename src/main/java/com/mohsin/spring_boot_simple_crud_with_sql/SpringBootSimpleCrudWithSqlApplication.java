629545f7eff3ff98d2f0e9b2bb273fe12cbe7dc3
Mohsin <mdmohsinhaider6@gmail.com>
2025-02-05 00:59:17 +0530

    Spring Boot

---
+++ src/main/java/com/mohsin/spring_boot_simple_crud_with_sql/SpringBootSimpleCrudWithSqlApplication.java
@@ -0,0 +1,12 @@
+package com.mohsin.spring_boot_simple_crud_with_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleCrudWithSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleCrudWithSqlApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mohsin/spring_boot_simple_crud_with_sql/SpringBootSimpleCrudWithSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohsin.spring_boot_simple_crud_with_sql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleCrudWithSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
