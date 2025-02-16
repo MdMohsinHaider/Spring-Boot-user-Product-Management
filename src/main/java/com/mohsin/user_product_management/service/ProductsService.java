4fb94e166aaf67b656cf4ffc21d8d15b370dbd2e
Mohsin <mdmohsinhaider6@gmail.com>
2025-02-17 03:56:06 +0530

    User product JavaDoc comments

--- src/main/java/com/mohsin/user_product_management/service/ProductsService.java
+++ src/main/java/com/mohsin/user_product_management/service/ProductsService.java
@@ -5,12 +5,60 @@ import com.mohsin.user_product_management.entities.Product;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Interface for Product Management Service.
+ * Defines CRUD operations for managing products.
+ *
+ * @author Md Mohsin
+ * @version 1.0
+ */
 public interface ProductsService {
 
+    /**
+     * Saves a new product in the database.
+     *
+     * @param product The product to be saved.
+     * @return The saved product with a generated ID.
+     */
     Product saveProduct(Product product);
+
+    /**
+     * Saves multiple products in the database in batch mode.
+     *
+     * @param products The list of products to be saved.
+     * @return The list of saved products.
+     */
     List<Product> saveMultipleProducts(List<Product> products);
+
+    /**
+     * Retrieves all available products from the database.
+     *
+     * @return A list of all products.
+     */
     List<Product> getAllProducts();
+
+    /**
+     * Fetches a product by its unique ID.
+     *
+     * @param id The ID of the product.
+     * @return An Optional containing the product if found, otherwise empty.
+     */
     Optional<Product> getProductById(int id);
+
+    /**
+     * Updates a product by its ID with new values.
+     *
+     * @param id The ID of the product to update.
+     * @param product The updated product details.
+     * @return An Optional containing the updated product if the update is successful.
+     */
     Optional<Product> updateProductById(int id, Product product);
+
+    /**
+     * Deletes a product by its ID.
+     *
+     * @param id The ID of the product to delete.
+     * @return true if the deletion was successful, false otherwise.
+     */
     boolean deleteProductById(int id);
 }
--- src/main/java/com/mohsin/user_product_management/service/UsersService.java
+++ src/main/java/com/mohsin/user_product_management/service/UsersService.java
@@ -5,17 +5,60 @@ import com.mohsin.user_product_management.entities.User;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Interface for User Management Service.
+ * Defines CRUD operations for managing users.
+ *
+ * @author Md Mohsin
+ * @version 1.0
+ */
 public interface UsersService {
 
+    /**
+     * Saves a new user in the database.
+     *
+     * @param user The user to be saved.
+     * @return The saved user with a generated ID.
+     */
     User saveUser(User user);
 
+    /**
+     * Saves multiple users in batch mode.
+     *
+     * @param users The list of users to be saved.
+     * @return The list of saved users.
+     */
     List<User> saveUsers(List<User> users);
 
+    /**
+     * Retrieves all users from the database.
+     *
+     * @return A list of all users.
+     */
     List<User> getAllUsers();
 
+    /**
+     * Fetches a user by their unique ID.
+     *
+     * @param id The ID of the user.
+     * @return An Optional containing the user if found, otherwise empty.
+     */
     Optional<User> getUserById(int id);
 
+    /**
+     * Updates a user by their ID with new details.
+     *
+     * @param id The ID of the user to update.
+     * @param user The updated user details.
+     * @return An Optional containing the updated user if the update is successful.
+     */
     Optional<User> updateUserById(int id, User user);
 
+    /**
+     * Deletes a user by their ID.
+     *
+     * @param id The ID of the user to delete.
+     * @return true if the deletion was successful, false otherwise.
+     */
     boolean deleteUserById(int id);
 }
